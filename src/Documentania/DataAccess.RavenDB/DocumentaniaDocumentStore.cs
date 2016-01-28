c13f3d4af43a36cb255c9f2c4d77a004d560c2cd
binaryfr3ak <binaryfr3ak@gmail.com>
2016-01-28 10:18:42 +0100

    Add project for ravendb

---
+++ src/Documentania/DataAccess.RavenDB/DocumentaniaDocumentStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.RavenDB
+{
+    using Raven.Client.Document;
+    public class DocumentaniaDocumentStore : DocumentStore
+    {
+        public DocumentaniaDocumentStore(string url, string defaultDatabase)
+            : base()
+        {
+            this.Url = url;
+            this.DefaultDatabase = defaultDatabase;
+        }
+    }
+}
