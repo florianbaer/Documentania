2af13f70ee8da6436f265fb1122724f05642a065
binaryfr3ak <binaryfr3ak@gmail.com>
2016-01-28 10:40:14 +0100

    Add RavenDB

---
+++ src/Documentania/DataAccess.RavenDB.Tests/DocumentaniaDocumentStoreTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccess.RavenDB.Tests
+{
+    using System.Security.Policy;
+    
+    using NUnit.Framework;
+
+    using Raven.Client;
+
+    using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+
+    /// <summary>
+    /// Summary description for DocumentaniaDocumentStoreTests
+    /// </summary>
+    [TestFixture]
+    public class DocumentaniaDocumentStoreTests
+    {
+        [Test]
+        public void TestCreateDocumentAndReadIt()
+        {
+            using (IDocumentStore store = new DocumentaniaDocumentStore("http://tromwn5930:8080/", "Documentania")) 
+            {
+                store.Initialize(); // initializes document store, by connecting to server and downloading various configurations
+
+                using (IDocumentSession session = store.OpenSession()) // opens a session that will work in context of 'DefaultDatabase'
+                {
+                    Document document = new Document() { Imported = DateTime.Now, Path = "asdf" };
+
+                    session.Store(document); // stores employee in session, assigning it to a collection `Employees`
+                    string documentId = document.Id; // Session.Store will assign Id to employee, if it is not set
+
+                    session.SaveChanges(); // sends all changes to server
+
+                    // Session implements Unit of Work pattern,
+                    // therefore employee instance would be the same and no server call will be made
+                    Document loadedEmployee = session.Load<Document>(documentId);
+                    Assert.Equals(document, loadedEmployee);
+                }
+            }
+        }
+      
+    }
+}
---
+++ src/Documentania/DataAccess.RavenDB/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.RavenDB
+{
+    public class Document
+    {
+        public virtual string Id { get; set; }
+
+        public virtual string Path { get; set; }
+        
+        public virtual DateTime Imported { get; set; }
+    }
+}
