724f9a59324de6c43882fe5ce5f2afdefe0b1e76
binaryfr3ak <binaryfr3ak@gmail.com>
2016-03-08 10:21:07 +0100

    Add filesystem notifier module

---
+++ src/FileSystemNotifier/FileSystemNotifyModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemNotifier
+{
+    using log4net;
+
+    using Microsoft.Practices.ServiceLocation;
+    using Microsoft.Practices.Unity;
+
+    using Prism.Modularity;
+
+    [Module(ModuleName = "FileSystemNotify")]
+    public class FileSystemNotifyModule : IModule
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(FileSystemNotifyModule));
+
+        private IUnityContainer container = ServiceLocator.Current.GetInstance<IUnityContainer>();
+
+        public void Initialize()
+        {
+            Log.Debug("FileSytemNotfier Module initialized");
+        }
+    }
+}
