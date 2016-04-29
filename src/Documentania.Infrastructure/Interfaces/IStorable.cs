0656cdcdf5e42280b51e4cd7ccb302e3ef6e78aa
binaryfr3ak <binaryfr3ak@gmail.com>
2016-04-29 09:19:31 +0200

    Remove contracts project

---
+++ src/Documentania.Infrastructure/Interfaces/IStorable.cs
@@ -0,0 +1,25 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="IStorable.cs" company="BaerDev">
+// // Copyright (c) BaerDev. All rights reserved.
+// // </copyright>
+// // <summary>
+// // The file 'IStorable.cs'.
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+
+namespace Documentania.Contracts
+{
+    /// <summary>
+    /// Interface for a storable object.
+    /// </summary>
+    public interface IStorable
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        string Id { get; set; }
+    }
+}
