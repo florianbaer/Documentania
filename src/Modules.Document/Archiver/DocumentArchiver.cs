6d3f49d8800b792f246e5c03544f584092afdcff
Florian Bär <binaryfr3ak@gmail.com>
2016-05-22 17:29:04 +0200

    Create metadata file inside of documentania

--- src/Modules.Document/Archiver/DocumentArchiver.cs
+++ src/Modules.Document/Archiver/DocumentArchiver.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace Modules.Document.Archiver
 {
     using System.IO;
+    using System.Reflection;
     using System.Xml;
+    using System.Xml.Linq;
     using System.Xml.Serialization;
 
     using ICSharpCode.SharpZipLib.Zip;
@@ -25,24 +27,33 @@ namespace Modules.Document.Archiver
                 Directory.CreateDirectory(commonAppData);
             }
 
-            var infoFile = Path.Combine(commonAppData, document.Name);
+            var infoFile = Path.Combine(commonAppData, document.Name + ".xml");
 
             XmlSerializer writer = new XmlSerializer(typeof(Document));
-
+            
             System.IO.FileStream file = System.IO.File.Create(infoFile);
 
             writer.Serialize(file, document);
             file.Close();
+
+            string metaData = Path.Combine(commonAppData, "Metadata" + ".xml");
+
+            XDocument doc = new XDocument(new XElement("Documentania",
+                new XElement("Version", new XAttribute("Version", Assembly.GetExecutingAssembly().GetName().Version))));
+
+            doc.Save(metaData);
 
             using (ZipFile zip = ZipFile.Create(Path.Combine(commonAppData, document.Id + ".document")))
             {
                 zip.BeginUpdate();
                 zip.Add(document.Path, Path.GetFileName(document.Path));
                 zip.Add(infoFile, Path.GetFileName(infoFile));
+                zip.Add(metaData, Path.GetFileName(metaData));
                 zip.CommitUpdate();
             }
 
             File.Delete(infoFile);
+            File.Delete(metaData);
         }
     }
 }
